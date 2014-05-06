3ef9e2ad6e4e3445c3017861b7db75adafb732e1
PaulineCrouillere <pcrouillere@gmail.com>
2014-05-06 22:21:00 +0200

    fix several bugs in database + create some test data

--- sql/jumper_base.sql
+++ sql/jumper_base.sql
@@ -4,32 +4,10 @@
 	/* ******************************************************************* */
 
 	/* ************* DROP TABLE ************* */
+	DROP TABLE jpTagMap;
 	DROP TABLE jpTag;
 	DROP TABLE jpUrl;
 	DROP TABLE jpUser;
-
-	/* * TAG * */
-	CREATE TABLE jpTag (
-	tagId INT NOT NULL AUTO_INCREMENT,
-	tagName VARCHAR(255) NOT NULL,
-	tagUrl
-	PRIMARY KEY (tagId)
-	);
-
-	/* * URL * */
-	CREATE TABLE jpUrl (
-	urlId INT NOT NULL AUTO_INCREMENT,
-	urlUri VARCHAR(255) UNIQUE NOT NULL,
-	PRIMARY KEY (urlId)
-	);
-	
-	/* * COUPLE TAG - URL **/
-	CREATE TABLE jpTagMap(
-	tagMapId INT NOT NULL AUTO_INCREMENT,
-	tagMapUrlId INT,
-	tagMapTagId INT,
-	PRIMARY KEY (tagMapId)
-	)
 
 	/* * USER * */
 	CREATE TABLE jpUser (
@@ -38,8 +16,37 @@
 	userPassword VARCHAR(255) NOT NULL,
 	PRIMARY KEY (userId)
 	);
+	
+	
+	/* * TAG * */
+	CREATE TABLE jpTag (
+	tagId INT NOT NULL AUTO_INCREMENT,
+	tagUserId INT,
+	tagName VARCHAR(255) NOT NULL,
+	PRIMARY KEY (tagId)
+	);
+
+	/* * URL * */
+	CREATE TABLE jpUrl (
+	urlId INT NOT NULL AUTO_INCREMENT,
+	urlUserId INT,
+	urlUri VARCHAR(255) UNIQUE NOT NULL,
+	PRIMARY KEY (urlId)
+	);
+	
+	/* * COUPLE TAG - URL **/
+	CREATE TABLE jpTagMap(
+	tagMapId INT NOT NULL AUTO_INCREMENT,
+	tagMapUserId INT,
+	tagMapUrlId INT,
+	tagMapTagId INT,
+	PRIMARY KEY (tagMapId)
+	);
 
 
 	/* * FOREIGN KEY * */
+	ALTER TABLE jpTag ADD CONSTRAINT jpTagfkUser FOREIGN KEY (tagUserId) REFERENCES jpUser(userId);
+	ALTER TABLE jpUrl ADD CONSTRAINT jpUrlfkUser FOREIGN KEY (urlUserId) REFERENCES jpUser(userId);
 	ALTER TABLE jpTagMap ADD CONSTRAINT jpTagMapFkUrl FOREIGN KEY (tagMapUrlId) REFERENCES jpUrl(urlId);
 	ALTER TABLE jpTagMap ADD CONSTRAINT jpTagMapFkTag FOREIGN KEY (tagMapTagId) REFERENCES jpTag(tagId);
+	ALTER TABLE jpTagMap ADD CONSTRAINT jpTagMapFkUser FOREIGN KEY (tagMapUserId) REFERENCES jpUser(userId);
