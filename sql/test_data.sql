d9a16cb7c66f37ba9579bf339ffbf59eb63ccdec
PaulineCrouillere <pcrouillere@gmail.com>
2014-05-08 19:00:30 +0200

    bug in test_data

--- sql/test_data.sql
+++ sql/test_data.sql
@@ -18,14 +18,14 @@ Insert into jpTag(tagUserId, tagName) values(1, 'DIY');
 Insert into jpTag(tagUserId, tagName) values(1, 'Cinema');
 
 
-Insert into jpUrl(urlUri) values(1, 'http://grooveshark.com/#!/masterofsoundtrack/broadcast');
-Insert into jpUrl(urlUri) values(1, 'http://lifehacker.com/5540019/five-best-bookmark-management-tools/all');
-Insert into jpUrl(urlUri) values(1, 'https://docs.google.com/forms/d/1Q8u2oobP1PhWBgsmvrDp61WWnKp9NF39qibDe0MmVv4/viewanalytics');
-Insert into jpUrl(urlUri) values(1, 'http://www.noomuseum.net/');
-Insert into jpUrl(urlUri) values(1, 'http://www.unclegram.fr/categories#photogram');
-Insert into jpUrl(urlUri) values(1, 'http://ecrans.liberation.fr/');
-Insert into jpUrl(urlUri) values(1, 'http://www.vertcerise.com/');
-Insert into jpUrl(urlUri) values(1, 'http://www.madmoizelle.com/films-2014-218292');
+Insert into jpUrl(urlUserId, urlUri) values(1, 'http://grooveshark.com/#!/masterofsoundtrack/broadcast');
+Insert into jpUrl(urlUserId, urlUri) values(1, 'http://lifehacker.com/5540019/five-best-bookmark-management-tools/all');
+Insert into jpUrl(urlUserId, urlUri) values(1, 'https://docs.google.com/forms/d/1Q8u2oobP1PhWBgsmvrDp61WWnKp9NF39qibDe0MmVv4/viewanalytics');
+Insert into jpUrl(urlUserId, urlUri) values(1, 'http://www.noomuseum.net/');
+Insert into jpUrl(urlUserId, urlUri) values(1, 'http://www.unclegram.fr/categories#photogram');
+Insert into jpUrl(urlUserId,urlUri) values(1, 'http://ecrans.liberation.fr/');
+Insert into jpUrl(urlUserId,urlUri) values(1, 'http://www.vertcerise.com/');
+Insert into jpUrl(urlUserId,urlUri) values(1, 'http://www.madmoizelle.com/films-2014-218292');
 
 
 insert into jpTagMap(tagMapUserId, tagMapUrlId, tagMapTagId) values(1, 1, 1);
