449877f87a59beec8568de0e2d5063f16eef9b2d
Pauline Cuche <pauline.cuche@gmail.com>
2014-05-29 18:28:48 +0200

    try get cookie

--- currentTab.js
+++ currentTab.js
@@ -1,6 +1,32 @@
 function currentTab() {
     chrome.tabs.getSelected(null, function(tab) {
 		alert(tab.url);
+		//alert(tab.title);
+		chrome.cookies.get({ url: 'http://localhost:8080/Jump/', name: 'JSESSIONID' },
+		  function (cookie) {
+			if (cookie) {
+			  alert(cookie.value);
+			  /*sucess = file('insertUrl.php?id=1&url='+tab.url+'&nomUrl='+tab.title));
+			  if(sucess== 2) alert("url ajouté");
+			  else(sucess == 1) alert("error ajout");*/
+			}
+			else {
+			  alert('Can\'t get cookie! Check the name!');
+			}
+		});
 	});	
  }
+ 
+ 
+function file(fichier)
+{
+	if(window.XMLHttpRequest) // FIREFOX
+	xhr_object = new XMLHttpRequest();
+	else return(false);
+	xhr_object.open("GET", fichier, true);
+	xhr_object.send();
+	if(xhr_object.readyState == 4) return (xhr_object.responseText);
+	else return(false);
+}
+	 
 document.getElementById('ajout').addEventListener("click", currentTab);
