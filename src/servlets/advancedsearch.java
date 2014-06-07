36732c6d09afdce209042dd87d5b198633653dce
Yiyan CHEN <metachenyiyan@gmail.com>
2014-06-07 19:23:38 +0200

    servelet for advanced search

---
+++ src/servlets/advancedsearch.java
@@ -0,0 +1,34 @@
+package servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import dao.Tag;
+import dao.Url;
+import dao.User;
+import framework.Session;
+
+public class advancedsearch extends HttpServlet {
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+		String req = request.getParameter("req");
+		PrintWriter out = response.getWriter();
+		Session session = new Session(request);
+		User user = (User) session.get_user();
+		List<Url> urls=user.advancedsearch(req); 
+		Iterator<Url> it = urls.iterator();
+		while (it.hasNext()){
+		   Url url = it.next(); // tu peux typer plus finement ici
+		   out.print(" $$$ "+url.getuUri());
+		}
+	}
+}
